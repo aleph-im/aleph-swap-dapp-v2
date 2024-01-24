ffb6de8fd183d604a48cc2cfb8988e367a1fbce8
Jonathan Schemoul <jonathan.schemoul@gmail.com>
2024-01-24 23:25:23 +0100

    removing history from swap page

--- src/pages/Swap.vue
+++ src/pages/Swap.vue
@@ -1,32 +1,16 @@
 <template>
   <q-page class="" padding>
     <div class="content">
-      <h4 class="linear-green">
+      <h4>
         Swap
       </h4>
-        <swap />
-      <h4 class="linear-green row justify-between">
-        History
-        <q-btn
-          color="white"
-          round
-          flat
-          dense
-          :icon="refreshing ? 'wait' : 'refresh'"
-          :active="!refreshing"
-          @click="get_swaps"
-        />
-      </h4>
-      <div v-for="swap of swaps" :key="swap.original_item_hash">
-        <swap-detail :swap="swap" />
-      </div>
+      <swap />
     </div>
   </q-page>
 </template>
 
 <script>
 import Swap from '../components/Swap.vue'
-import SwapDetail from '../components/SwapDetail.vue'
 // import { mapState } from 'vuex'
 import { posts } from 'aleph-js'
 export default {
@@ -36,29 +20,15 @@ export default {
   data() {
     return {
       api_server: 'https://api2.aleph.im',
-      swap_address: '0xED9d5B040386F394B9ABd34fD59152756b126710',
-      swaps: [],
-      refreshing: false
+      swap_address: '0xED9d5B040386F394B9ABd34fD59152756b126710'
     }
   },
   components: {
-    Swap,
-    SwapDetail
+    Swap
   },
   methods: {
-    async get_swaps() {
-      this.refreshing = true
-      let result = await posts.get_posts('xchain-swap', {
-        addresses: [this.swap_address],
-        api_server: this.api_server
-      })
-      console.log(result)
-      this.swaps = result.posts
-      this.refreshing = false
-    }
   },
   mounted() {
-    this.get_swaps()
   }
 }
 </script>
